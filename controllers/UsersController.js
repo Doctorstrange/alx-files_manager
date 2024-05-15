b3e97b98eee820ae353006a2c198d6194da516b3
doctorstrange <doctorstrange@gmail.com>
2024-05-15 17:49:57 +0000

    4. Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -33,6 +33,23 @@ class UsersController {
       response.status(500).json({ error: 'Server error' });
     }
   }
+
+static async getMe (request, response) {
+    try {
+      const userToken = request.header('X-Token');
+      const authKey = `auth_${userToken}`;
+      const userID = await redisClient.get(authKey);
+      console.log('USER KEY GET ME', userID);
+      if (!userID) {
+        response.status(401).json({ error: 'Unauthorized' });
+      }
+      const user = await dbClient.getUser({ _id: ObjectId(userID) });
+      response.json({ id: user._id, email: user.email });
+    } catch (error) {
+      console.log(error);
+      response.status(500).json({ error: 'Server error' });
+    }
+  }
 }
 
 export default UsersController;
