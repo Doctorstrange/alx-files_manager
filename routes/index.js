523c737d3a9634e5df2b3f4fa1ec5adc45735b5d
doctorstrange <doctorstrange@gmail.com>
2024-05-15 18:50:19 +0000

    5. First file

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController.js';
-import AuthController from '../controllers/AuthController.js';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
-const routeController = (app) => {
+const routing = (app) => {
   app.use('/', router);
 
   router.get('/status', (req, res) => {
@@ -17,7 +18,7 @@ const routeController = (app) => {
   });
 
   router.post('/users', (req, res) => {
-  UsersController.postNew(req, res);
+    UsersController.postNew(req, res);
   });
 
   router.get('/connect', (req, res) => {
@@ -31,6 +32,30 @@ const routeController = (app) => {
   router.get('/users/me', (req, res) => {
     UsersController.getMe(req, res);
   });
+
+  router.post('/files', (req, res) => {
+    FilesController.postUpload(req, res);
+  });
+
+  router.get('/files/:id', (req, res) => {
+    FilesController.getShow(req, res);
+  });
+
+  router.get('/files', (req, res) => {
+    FilesController.getIndex(req, res);
+  });
+
+  router.put('/files/:id/publish', (req, res) => {
+    FilesController.putPublish(req, res);
+  });
+
+  router.put('/files/:id/unpublish', (req, res) => {
+    FilesController.putUnpublish(req, res);
+  });
+
+  router.post('/files/:id/data', (req, res) => {
+    FilesController.getFile(req, res);
+  });
 };
 
-export default routeController;
+export default routing;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import controllerRouting from './routes/index';
+import routing from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
-controllerRouting(app);
+routing(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
