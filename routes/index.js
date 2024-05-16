4d5c0b040e92b8b24fe559cc554854d394718ddf
doctorstrange <doctorstrange@gmail.com>
2024-05-16 02:51:07 +0000

    4. Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', basicAuthenticate, AuthController.getConnect);
+router.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+router.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
 export default router;
