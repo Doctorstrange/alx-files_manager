27f1aa4e1b1d0a48da573b8406489599a92b6c56
doctorstrange <doctorstrange@gmail.com>
2024-05-16 03:06:03 +0000

    4. Authenticate a user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,53 +4,45 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AuthController {
-  static async getConnect (request, response) {
-    const authHeader = request.headers.authorization;
+  static async getConnect(req, res) {
+    const authHeader = req.headers.authorization;
     if (!authHeader) {
-      response.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
     try {
       const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
       const email = auth[0];
-      const pass = sha1(auth[1]);
+      const password = sha1(auth[1]);
 
       const user = await dbClient.getUser({ email });
-      // console.log('USER IN AUTH GETCONNECT()', user);
-
-      if (!user) {
-        response.status(401).json({ error: 'Unauthorized' });
-      }
-
-      if (pass !== user.password) {
-        response.status(401).json({ error: 'Unauthorized' });
+      if (!user || password !== user.password) {
+        return res.status(401).json({ error: 'Unauthorized' });
       }
 
       const token = uuidv4();
       const key = `auth_${token}`;
-      const duration = (60 * 60 * 24);
+      const duration = 60 * 60 * 24; // 24 hours
       await redisClient.set(key, user._id.toString(), duration);
 
-      response.status(200).json({ token });
+      return res.status(200).json({ token });
     } catch (err) {
       console.log(err);
-      response.status(500).json({ error: 'Server error' });
+      return res.status(500).json({ error: 'Server error' });
     }
   }
 
-  static async getDisconnect (request, response) {
+  static async getDisconnect(req, res) {
     try {
-      const userToken = request.header('X-Token');
-      // console.log('USER TOKEN DISCONNECT', userToken);
+      const userToken = req.header('X-Token');
       const userKey = await redisClient.get(`auth_${userToken}`);
-      // console.log('USER KEY DISCONNECT', userKey);
       if (!userKey) {
-        response.status(401).json({ error: 'Unauthorized' });
+        return res.status(401).json({ error: 'Unauthorized' });
       }
       await redisClient.del(`auth_${userToken}`);
-      response.status(204).send('Disconnected');
+      return res.status(204).send();
     } catch (err) {
       console.log(err);
-      response.status(500).json({ error: 'Server error' });
+      return res.status(500).json({ error: 'Server error' });
     }
   }
 }
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,13 +4,13 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class UsersController {
-  static async postNew(request, response) {
-    const { email, password } = request.body;
+  static async postNew(req, res) {
+    const { email, password } = req.body;
     if (!email) {
-      response.status(400).json({ error: 'Missing email' });
+      return res.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      response.status(400).json({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing password' });
     }
 
     const hashPwd = sha1(password);
@@ -20,39 +20,35 @@ class UsersController {
       const user1 = await collection.findOne({ email });
 
       if (user1) {
-        response.status(400).json({ error: 'Already exist' });
+        return res.status(400).json({ error: 'Already exist' });
       } else {
-        collection.insertOne({ email, password: hashPwd });
+        await collection.insertOne({ email, password: hashPwd });
         const newUser = await collection.findOne(
-          { email }, { projection: { email: 1 } },
+          { email }, { projection: { email: 1 } }
         );
-        response.status(201).json({ id: newUser._id, email: newUser.email });
+        return res.status(201).json({ id: newUser._id, email: newUser.email });
       }
     } catch (error) {
       console.log(error);
-      response.status(500).json({ error: 'Server error' });
+      return res.status(500).json({ error: 'Server error' });
     }
   }
 
-  static async getMe(request, response) {
+  static async getMe(req, res) {
     try {
-      const userToken = request.header('X-Token');
+      const userToken = req.header('X-Token');
       const authKey = `auth_${userToken}`;
-      // console.log('USER TOKEN GET ME', userToken);
       const userID = await redisClient.get(authKey);
-      console.log('USER KEY GET ME', userID);
       if (!userID) {
-        response.status(401).json({ error: 'Unauthorized' });
+        return res.status(401).json({ error: 'Unauthorized' });
       }
       const user = await dbClient.getUser({ _id: ObjectId(userID) });
-      // console.log('USER GET ME', user);
-      response.json({ id: user._id, email: user.email });
+      return res.json({ id: user._id, email: user.email });
     } catch (error) {
       console.log(error);
-      response.status(500).json({ error: 'Server error' });
+      return res.status(500).json({ error: 'Server error' });
     }
   }
 }
 
 export default UsersController;
-// export default postNew();
