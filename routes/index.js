348b2c41733fe8930d7c6813e1a008a11c873e35
doctorstrange <doctorstrange@gmail.com>
2024-05-16 02:04:52 +0000

    2. First API

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
-import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
+
+
 
 const router = Router();
 
